945d6b67b48c3b7d3fb29247fac9749c974c4a60
freezy <freezy@kodi.tv>
2017-10-03 20:44:15 +0200

    Added pug config.

---
+++ config/auth.js
@@ -0,0 +1,15 @@
+module.exports = {
+	local: true,
+	github: true,
+	google: true,
+	ipboard: [
+		{
+			enabled: true,
+			id: 'vpf',
+			name: 'VPF',
+			icon: 'vpf',
+			baseURL: 'http://127.0.0.1:8088/index.php',
+			clientID: '7e296c231456d2c37e0a',
+		}
+	]
+};
