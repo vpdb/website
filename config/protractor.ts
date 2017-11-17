34b1d825a3c11431eda9d62880d58dcce4385546
freezy <freezy@kodi.tv>
2017-11-18 00:50:51 +0100

    Register root user before running test suite.

--- config/protractor.ts
+++ config/protractor.ts
@@ -1,4 +1,5 @@
-import { Config } from 'protractor';
+import { browser, by, Config } from 'protractor';
+import { LoginModalPage } from "../src/app/auth/login.modal.page";
 
 export let config: Config = {
 	framework: 'jasmine',
@@ -9,5 +10,16 @@ export let config: Config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 	params: {
 		url: 'http://localhost:3333'
+	},
+	onPrepare: () => {
+		// register root user
+		browser.driver.get('http://localhost:3333');
+		browser.driver.findElement(by.id('login-btn')).click();
+		browser.driver.findElement(by.id('login-toggle')).click();
+		browser.driver.findElement(by.id('register-email')).sendKeys(LoginModalPage.EMAIL);
+		browser.driver.findElement(by.id('register-username')).sendKeys(LoginModalPage.USERNAME);
+		browser.driver.findElement(by.id('register-password')).sendKeys(LoginModalPage.PASSWORD);
+		browser.driver.findElement(by.id('register-submit')).click();
+		browser.driver.findElement(by.id('dismiss-button')).click();
 	}
 };
