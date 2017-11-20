a5b75f9aea975d60a8189a61ee4c2a9d197c27c2
freezy <freezy@kodi.tv>
2017-11-21 00:23:22 +0100

    Added BrowserStack integration.

---
+++ config/protractor.browserstack.ts
@@ -0,0 +1,14 @@
+import { merge } from 'lodash';
+import { Config } from 'protractor';
+
+import { config as commonConfig } from './protractor.common';
+
+
+export let config:Config = merge(commonConfig, {
+	seleniumAddress: 'http://hub-cloud.browserstack.com/wd/hub',
+	capabilities: {
+		'browserstack.user': process.env.BROWSERSTACK_USER,
+		'browserstack.key': process.env.BROWSERSTACK_KEY
+	},
+});
+
