63ed7538691632802766940e488c5d011ea49c86
freezy <freezy@kodi.tv>
2017-11-28 00:04:33 +0100

    Fixed Safari version in tests to 11.

--- config/protractor.browserstack.ts
+++ config/protractor.browserstack.ts
@@ -1,24 +1,24 @@
-import { merge } from 'lodash';
+import { merge, isObject, assign } from 'lodash';
 import { browser, Config } from 'protractor';
 import { FileDetector } from 'selenium-webdriver/remote';
 
 import { config as commonConfig, setupReporter, setupUsers } from './protractor.common';
 
-function capability(browser:string) {
-	return {
-		'browserName': browser,
+function capability(browser:any) {
+	const base = {
 		'browserstack.local': true,
 		'browserstack.user': process.env.BROWSERSTACK_USER,
 		'browserstack.key': process.env.BROWSERSTACK_KEY,
 		'build': 'Codeship build ' + process.env.CI_BUILD_NUMBER,
 		'project': 'vpdb/website'
-	}
+	};
+	return assign(base, isObject(browser) ? browser : { browserName: browser });
 }
 
 export let config: Config = merge(commonConfig, {
 	seleniumAddress: 'http://hub-cloud.browserstack.com/wd/hub',
 	capabilities: null,
-	multiCapabilities: [ 'Chrome', 'Safari', 'Firefox', 'IE' ].map(capability),
+	multiCapabilities: [ 'Chrome', 'Firefox', 'IE', { os: 'OS X', os_version: 'High Sierra', browserName: 'Safari', browser_version: '11.0' } ].map(capability),
 	onPrepare: () => {
 		setupReporter();
 		console.log('Setting file detector to remote.'); // https://www.browserstack.com/automate/node
