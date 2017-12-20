0ffc3ee5b5592567e88425913244eb2878de624a
freezy <freezy@kodi.tv>
2017-12-20 01:08:43 +0100

    test: Add tests for select author modal.

---
+++ src/app/users/author.select.modal.page.ts
@@ -0,0 +1,63 @@
+import { by, element, ElementArrayFinder } from 'protractor';
+import { promise } from 'selenium-webdriver';
+import { BasePage } from '../../test/BasePage';
+
+export class AuthorSelectModalPage extends BasePage {
+
+	private searchBox = element(by.id('user-search'));
+	private searchResult = element(by.css('#user-search + ul.dropdown-menu'));
+	private searchResults = this.searchResult.all(by.repeater('match in matches track by $index'));
+	private submitButton = element(by.id('submit-btn'));
+	private addedRoles = element.all(by.repeater('r in vm.roles'));
+
+	search(name: string) {
+		this.searchBox.sendKeys(name);
+	}
+
+	hasSearchResults(): promise.Promise<boolean> {
+		return this.searchResults.count().then(count => count > 0);
+	}
+
+	selectSearchResult(position: number) {
+		this.searchResults.get(position).click();
+	}
+
+	getSelectedName(): promise.Promise<string> {
+		return this.searchBox.getAttribute('value');
+	}
+
+	setRole(title: string) {
+		element(by.id('role')).sendKeys(title);
+	}
+
+	addRole(title: string = '') {
+		if (title) {
+			this.setRole(title);
+		}
+		element(by.id('add-btn')).click();
+	}
+
+	removeRole(title:string) {
+		this.findRole(title).then(roles => roles[0].element(by.css('.a')).click());
+	}
+
+	hasRole(title: string): promise.Promise<boolean> {
+		return this.findRole(title).then(roles => roles.length === 1);
+	}
+
+	getSubmitButtonText(): promise.Promise<string> {
+		return this.submitButton.getText();
+	}
+
+	dismiss() {
+		element(by.id('cancel-btn')).click();
+	}
+
+	submit() {
+		this.submitButton.click();
+	}
+
+	private findRole(title:string): ElementArrayFinder {
+		return this.addedRoles.filter(r => r.getText().then(role => role === title + '×'));
+	}
+}
---
+++ src/app/users/author.select.modal.spec.ts
@@ -0,0 +1,77 @@
+import { browser } from 'protractor';
+import { Games } from '../../test/backend/Games';
+import { Game } from '../../test/models/Game';
+import { AppPage } from '../app.page';
+import { ReleaseAddPage } from '../releases/release.add.page';
+import { AuthorSelectModalPage } from './author.select.modal.page';
+
+describe('Add author', () => {
+
+	const appPage = new AppPage();
+	const releaseAddPage = new ReleaseAddPage();
+	const authorSelectModal = new AuthorSelectModalPage();
+	const games:Games = new Games(browser.params.vpdb);
+
+	let game:Game;
+
+	beforeAll(() => {
+		return games.createGame().then((g:Game) => {
+			game = g;
+			releaseAddPage.get(game);
+		});
+	});
+
+	beforeEach(() => {
+		releaseAddPage.addAuthor();
+	});
+
+
+	afterAll(() => {
+		appPage.logout();
+	});
+
+	it('should display validation errors');
+
+	it('should find a member', () => {
+		authorSelectModal.search('memb');
+		expect(authorSelectModal.hasSearchResults()).toBe(true);
+
+		authorSelectModal.dismiss();
+	});
+
+	it('should select a member', () => {
+		authorSelectModal.search('memb');
+		authorSelectModal.selectSearchResult(0);
+		expect(authorSelectModal.getSelectedName()).toBe('member');
+
+		authorSelectModal.dismiss();
+	});
+
+	it('should add a role', () => {
+		authorSelectModal.addRole('superhero');
+		expect(authorSelectModal.hasRole('superhero')).toBe(true);
+
+		authorSelectModal.dismiss();
+	});
+
+	it('should add multiple roles', () => {
+		authorSelectModal.addRole('idiot');
+		authorSelectModal.addRole('gros con');
+		expect(authorSelectModal.hasRole('idiot')).toBe(true);
+		expect(authorSelectModal.hasRole('gros con')).toBe(true);
+
+		authorSelectModal.dismiss();
+	});
+
+	it('should remove a role', () => {
+		authorSelectModal.addRole('hangman');
+		expect(authorSelectModal.hasRole('hangman')).toBe(true);
+
+		authorSelectModal.removeRole('hangman')
+		expect(authorSelectModal.hasRole('hangman')).toBe(false);
+
+		authorSelectModal.dismiss();
+	});
+
+
+});
