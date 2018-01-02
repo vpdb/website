912436fc0ef90a76db83fe27ee03dedc07008af8
freezy <freezy@kodi.tv>
2018-01-03 00:54:05 +0100

    test: Remove Firefox and Safari from BrowserStack until driver works correctly.

--- config/protractor.browserstack.ts
+++ config/protractor.browserstack.ts
@@ -20,12 +20,12 @@ export let config: Config = merge(commonConfig, {
 	capabilities: null,
 	multiCapabilities: [
 			{ browserName: 'Chrome', os: 'Windows' },
-			{ browserName: 'Firefox', os: 'Windows' },
+			//{ browserName: 'Firefox', os: 'Windows' },
 			{ browserName: 'Chrome', os: 'OS X' },
-			{ browserName: 'Firefox', os: 'OS X' },
+			//{ browserName: 'Firefox', os: 'OS X' },
 			//{ browserName: 'Edge', browser_version: '16.0' },
 			//{ browserName: 'IE', browser_version: '11.0' },
-			{ browserName: 'Safari', browser_version: '11.0', os: 'OS X', os_version: 'High Sierra' }
+			//{ browserName: 'Safari', browser_version: '11.0', os: 'OS X', os_version: 'High Sierra' }
 		].map(capability),
 	onPrepare: () => {
 		setupReporter();
