7d06cc33493ce557fb02730535d0b9c022fec7ed
freezy <freezy@kodi.tv>
2018-01-30 00:52:07 +0100

    fix: Handle time-ago in the future correctly.

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -24,3 +24,26 @@
 export function msdElasticConfig(msdElasticConfig) {
 	msdElasticConfig.append = '\n\n';
 }
+
+export function timeAgoConfig(timeAgoSettings) {
+	timeAgoSettings.allowFuture = true;
+	timeAgoSettings.overrideLang = 'en_US';
+	timeAgoSettings.strings.en_US = {
+		prefixAgo: null,
+		prefixFromNow: 'in',
+		suffixAgo: 'ago',
+		suffixFromNow: null,
+		seconds: 'less than a minute',
+		minute: 'about a minute',
+		minutes: '%d minutes',
+		hour: 'about an hour',
+		hours: 'about %d hours',
+		day: 'a day',
+		days: '%d days',
+		month: 'about a month',
+		months: '%d months',
+		year: 'about a year',
+		years: '%d years',
+		numbers: []
+	};
+}
