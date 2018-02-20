72ac17aefe057dbe734d7ff5663a265745115b17
freezy <freezy@kodi.tv>
2018-02-20 01:25:46 +0100

    chore(deployment): Add git hooks and read config from ENV.

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 const { resolve } = require('path');
+const { existsSync } = require('fs');
 
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -9,6 +10,9 @@ const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 const appContext = resolve(__dirname, '../src/app');
 const staticContext = resolve(__dirname, '../src/static');
 const iconsContext = resolve(__dirname, '../src/icons');
+const websiteConfig = process.env.WEBSITE_CONFIG && existsSync(process.env.WEBSITE_CONFIG)
+	? process.env.WEBSITE_CONFIG
+	: './vpdb.' + process.env.CONFIG + '.json';
 
 module.exports  = function(options) {
 	const isProd = options.env === 'prod';
@@ -74,7 +78,7 @@ module.exports  = function(options) {
 			}),
 
 			new webpack.DefinePlugin({
-				WEBSITE_CONFIG: JSON.stringify(require('./vpdb.' + process.env.CONFIG + '.json'))
+				WEBSITE_CONFIG: JSON.stringify(require(websiteConfig))
 			})
 		],
 		output: {
