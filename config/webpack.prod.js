27ce1d56d2edf4b7396ab4529ae45c2e1c0a7b71
freezy <freezy@kodi.tv>
2018-02-22 00:20:51 +0100

    feat(build): Update pre-cache settings.

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -29,21 +29,55 @@ module.exports = function(options) {
 				cacheId: 'vpdb',
 				globDirectory: options.outputPath,
 				globPatterns: ['**/*.{html,js,css,jpg,png,eot,svg,ttf,woff}'],
-				globIgnores: [ 'sprite.svg', '**/glyphicons-*' ],
+				globIgnores: ['sprite.svg', '**/glyphicons-*'],
 				swDest: join(options.outputPath, 'sw.js'),
 				clientsClaim: true,
 				skipWaiting: true,
 				dontCacheBustUrlsMatching: /-([0-9a-f]{12}|[0-9A-Za-z]{8})\./,
 				navigateFallback: options.websiteUrl + 'index.html',
+				ignoreUrlParametersMatching: [/^utm_/, /^_$/],
 				runtimeCaching: [{
-					urlPattern: options.apiUrl,
-					handler: 'networkFirst'
+					urlPattern: new RegExp('^' + regexEscape(options.apiUrl)),
+					handler: 'networkFirst',
+					options: {
+						cacheName: 'api-cache',
+						networkTimeoutSeconds: 10,
+						expiration: {
+							maxAgeSeconds: 3600,
+						},
+					}
 				}, {
-					urlPattern: options.storageUrl,
-					handler: 'cacheFirst'
+					urlPattern: new RegExp('^' + regexEscape(options.storageUrl)),
+					handler: 'cacheFirst',
+					options: {
+						cacheName: 'storage-cache',
+						cacheExpiration: {
+							maxAgeSeconds: 3600 * 24 * 7 // cache storage one week
+						}
+					}
+				}, {
+					urlPattern: /https?:\/\/p\.typekit\.net/,
+					handler: 'cacheFirst',
+					options: {
+						cacheName: 'p-typekit-cache',
+						cacheExpiration: {
+							maxAgeSeconds: 3600 * 24 * 7 // one week
+						}
+					}
+				}, {
+					urlPattern: /https?:\/\/use\.typekit\.net/,
+					handler: 'cacheFirst',
+					options: {
+						cacheName: 'use-typekit-cache',
+						cacheExpiration: {
+							maxAgeSeconds: 3600 * 24 * 7 // one week
+						},
+						cacheableResponse: {
+							statuses: [200, 307],
+						},
+					}
 				}]
 			})
-
 		],
 
 		output: {
@@ -63,3 +97,7 @@ module.exports = function(options) {
 		}
 	});
 };
+
+function regexEscape(s) {
+	return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+}
