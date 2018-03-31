4857e21d45bd1a61562bd096f2cd37c853d5d4ce
freezy <freezy@kodi.tv>
2018-03-31 23:16:08 +0200

    feat(app): Preload critical assets.

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -7,6 +7,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
+const PreloadWebpackPlugin = require('preload-webpack-plugin');
 
 const srcContext = resolve(__dirname, '../src');
 const appContext = resolve(__dirname, '../src/app');
@@ -16,6 +17,27 @@ const iconsContext = resolve(__dirname, '../src/icons');
 module.exports  = function(options) {
 	const isProd = options.env === 'prod';
 	const cssLoader = { loader: 'css-loader', options: { sourceMap: true, minimize: isProd } };
+	const preConnect = [];
+
+	// pre-connect to api and storage on every page
+	if (options.websiteConfig.webUri.hostname !== options.websiteConfig.apiUri.hostname || options.websiteConfig.webUri.port !== options.websiteConfig.apiUri.port) {
+		preConnect.push(options.websiteConfig.apiUri.protocol + '://' + options.websiteConfig.storageUri.hostname + ([80, 443].includes(options.websiteConfig.storageUri.port) ? '' : ':' + options.websiteConfig.storageUri.port))
+	}
+	if ((options.websiteConfig.webUri.hostname !== options.websiteConfig.storageUri.hostname || options.websiteConfig.webUri.port !== options.websiteConfig.storageUri.port) &&
+		(options.websiteConfig.apiUri.hostname !== options.websiteConfig.storageUri.hostname || options.websiteConfig.apiUri.port !== options.websiteConfig.storageUri.port)) {
+		preConnect.push(options.websiteConfig.storageUri.protocol + '://' + options.websiteConfig.storageUri.hostname + ([80, 443].includes(options.websiteConfig.storageUri.port) ? '' : ':' + options.websiteConfig.storageUri.port))
+	}
+	preConnect.push('//www.gravatar.com');
+
+	// pre-load fonts
+	const preLoad = [
+		{ href: 'https://use.typekit.net/af/e4603e/0000000000000000000132e3/27/l?subset_id=2&fvd=n7&v=3', as: 'font' },
+		{ href: 'https://use.typekit.net/af/35c51c/0000000000000000000132e4/27/l?subset_id=2&fvd=i7&v=3', as: 'font' },
+		{ href: 'https://use.typekit.net/af/236da1/0000000000000000000132db/27/l?subset_id=2&fvd=n1&v=3', as: 'font' },
+		{ href: 'https://use.typekit.net/af/6a0e7c/0000000000000000000132dc/27/l?subset_id=2&fvd=i1&v=3', as: 'font' },
+		{ href: 'https://use.typekit.net/af/62b3d3/0000000000000000000132dd/27/l?subset_id=2&fvd=n3&v=3', as: 'font' },
+		{ href: 'https://use.typekit.net/af/6c39dc/0000000000000000000132de/27/l?subset_id=2&fvd=i3&v=3', as: 'font' },
+	];
 	return {
 		entry: {
 			app: './src/index.js'
@@ -57,7 +79,22 @@ module.exports  = function(options) {
 				template: './src/index.pug',
 				inject: false,
 				// see https://github.com/filamentgroup/loadCSS
-				loadCss: UglifyJS.minify(readFileSync('./node_modules/fg-loadcss/src/cssrelpreload.js').toString()).code
+				loadCss: UglifyJS.minify(readFileSync('./node_modules/fg-loadcss/src/cssrelpreload.js').toString()).code,
+				preConnect: preConnect,
+				preLoad: preLoad
+			}),
+
+			new PreloadWebpackPlugin({
+				rel: 'preload',
+				include: 'allAssets',
+				// this leaves *.woff and background_blurred*.jpg
+				fileBlacklist: [ /^((?!((app|vendor)\.bundle-[a-f0-9]{12}\.js|app-[a-zA-Z0-9]{8}\.html|\.woff)).)*$/, /\.map$/ ],
+				as(entry) {
+					if (/\.woff$/.test(entry)) return 'font';
+					if (/\.jpg/.test(entry)) return 'image';
+					if (/\.html/.test(entry)) return 'fetch';
+					return 'script';
+				}
 			}),
 
 			new SpriteLoaderPlugin({
