5291fde93e9a748f37a8d3344ea53b82693cb562
freezy <freezy@kodi.tv>
2019-04-27 01:14:22 +0200

    chore: Fix linting issues.

--- src/app/games/admin/add/game.add.admin.ctrl.js
+++ src/app/games/admin/add/game.add.admin.ctrl.js
@@ -17,7 +17,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-import angular from 'angular';
 import { omit } from 'lodash';
 
 export default class GameAddAdminCtrl {
