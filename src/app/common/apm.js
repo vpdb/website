d4af9ee6b3d581616ee1f59b3fdf3e894d2fb518
freezy <freezy@kodi.tv>
2019-05-22 00:05:04 +0200

    apm: Fix transaction names of initial route.

--- src/app/common/apm.js
+++ src/app/common/apm.js
@@ -17,7 +17,14 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-import { init as initApm } from '@elastic/apm-rum';
+import {init as initApm} from '@elastic/apm-rum';
+import {BACKGLASS_ADD} from '../backglasses/admin/backglass.admin.states';
+import {CONFIRM_TOKEN, RESET_PASSWORD} from './common.states';
+import {GAME_DETAILS} from '../games/game.states';
+import {GAME_ADMIN_EDIT} from '../games/admin/game.admin.states';
+import {RELEASE_DETAILS} from '../releases/release.states';
+import {RELEASE_ADD, RELEASE_EDIT, RELEASE_VERSION_ADD} from '../releases/admin/release.admin.states';
+import {VPT_PREVIEW} from '../releases/details/vpt/vpt.states';
 
 let apm = false;
 
@@ -25,6 +32,7 @@ let apm = false;
 const rumConfig = WEBSITE_CONFIG.rum;
 if (rumConfig && rumConfig.elastic) {
 	apm = initApm({
+		pageLoadTransactionName: pathToName(window.location.pathname),
 		// eslint-disable-next-line
 		serviceName: WEBSITE_CONFIG.name,
 		// eslint-disable-next-line
@@ -36,8 +44,29 @@ if (rumConfig && rumConfig.elastic) {
 			// eslint-disable-next-line
 			WEBSITE_CONFIG.storageUri.protocol + '://' + WEBSITE_CONFIG.storageUri.hostname + ':' + WEBSITE_CONFIG.storageUri.port,
 		],
+		flushInterval: 5000,
 	});
-	apm.setInitialPageLoadName('Init');
 }
 
 export default apm;
+
+function pathToName(path) {
+	const regexes = {
+		[BACKGLASS_ADD.url]: /^\/games\/[^/]+\/add-backglass$/,
+		[CONFIRM_TOKEN.url]: /^\/confirm\/[^/]+$/,
+		[RESET_PASSWORD.url]: /^\/reset-password\/[^/]+$/,
+		[GAME_DETAILS.url]: /^\/games\/[^/]+$/,
+		[GAME_ADMIN_EDIT.url]: /^\/games\/[^/]+\/edit$/,
+		[RELEASE_DETAILS.url]: /^\/games\/[^/]+\/releases\/[^/]+$/,
+		[RELEASE_ADD.url]: /^\/games\/[^/]+\/add-release$/,
+		[RELEASE_VERSION_ADD.url]: /^\/games\/[^/]+\/releases\/[^/]+\/add$/,
+		[RELEASE_EDIT.url]: /^\/games\/[^/]+\/releases\/[^/]+\/edit$/,
+		[VPT_PREVIEW.url]: /^\/games\/[^/]+\/releases\/[^/]+\/[^/]+\/[^/]+\/preview$/,
+	};
+	for (const key of Object.keys(regexes)) {
+		if (regexes[key].test(path)) {
+			return key;
+		}
+	}
+	return path;
+}
